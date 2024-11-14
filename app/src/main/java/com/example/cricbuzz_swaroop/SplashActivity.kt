8b88a4538797f3eeef8068f6b0727a989831addf
Swaroopkarneddi <swaroopweb07@gmail.com>
2024-11-14 13:31:17 +0530

    scorecard

--- app/src/main/java/com/example/cricbuzz_swaroop/SplashActivity.kt
+++ app/src/main/java/com/example/cricbuzz_swaroop/SplashActivity.kt
@@ -11,24 +11,20 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        // Wait for 2 seconds before navigating to the main activity or login
         Handler().postDelayed({
-            val isLoggedIn = checkIfUserIsLoggedIn()  // Check if the user is logged in
+            val isLoggedIn = checkIfUserIsLoggedIn()
 
             val intent = if (isLoggedIn) {
-                // If logged in, navigate to the WelcomeActivity (instead of MainActivity)
                 Intent(this, WelcomeActivity::class.java)
             } else {
-                // If not logged in, navigate to the LoginActivity
                 Intent(this, LoginActivity::class.java)
             }
             startActivity(intent)
-            finish()  // Close the Splash Activity
-        }, 2000) // 2-second delay
+            finish()
+        }, 2000)
     }
 
     private fun checkIfUserIsLoggedIn(): Boolean {
-        // Check login status from SharedPreferences
         val sharedPreferences = getSharedPreferences("user_prefs", MODE_PRIVATE)
         return sharedPreferences.getBoolean("isLoggedIn", false)
     }
